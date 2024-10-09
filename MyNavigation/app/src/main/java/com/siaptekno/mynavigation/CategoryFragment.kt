e2b6489f3f44ae966dcaecc408632ea64d0ec696
haikal <asmanhaikal@gmail.com>
2024-10-09 08:54:10 +0700

    feat: Navigation - change sending data with Bundle to sending data with SafeArgs

--- MyNavigation/app/src/main/java/com/siaptekno/mynavigation/CategoryFragment.kt
+++ MyNavigation/app/src/main/java/com/siaptekno/mynavigation/CategoryFragment.kt
@@ -31,10 +31,15 @@ class CategoryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnCategoryLifestyle.setOnClickListener { view ->
-            val mBundle = Bundle()
+//            OLD CODES - Using bundle to send data
+/*            val mBundle = Bundle()
             mBundle.putString(EXTRA_NAME, "Lifestyle")
             mBundle.putLong(EXTRA_STOCK, 7)
-            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)
+            view.findNavController().navigate(R.id.action_categoryFragment_to_detailCategoryFragment, mBundle)*/
+            val toDetailCategoryFragment = CategoryFragmentDirections.actionCategoryFragmentToDetailCategoryFragment()
+            toDetailCategoryFragment.name = "lifestyle"
+            toDetailCategoryFragment.stock = 7
+            view.findNavController().navigate(toDetailCategoryFragment)
         }
     }
 
