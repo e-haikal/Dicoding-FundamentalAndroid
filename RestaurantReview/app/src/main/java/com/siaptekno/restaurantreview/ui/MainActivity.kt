24bc136eb32110baa1646e6fa73665d889616ee4
haikal <asmanhaikal@gmail.com>
2024-10-11 13:23:09 +0700

    feat: adding codelab - sending data review

--- RestaurantReview/app/src/main/java/com/siaptekno/restaurantreview/ui/MainActivity.kt
+++ RestaurantReview/app/src/main/java/com/siaptekno/restaurantreview/ui/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.siaptekno.restaurantreview.ui
 
+import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +14,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.siaptekno.restaurantreview.R
 import com.siaptekno.restaurantreview.data.response.CustomerReviewsItem
+import com.siaptekno.restaurantreview.data.response.PostReviewResponse
 import com.siaptekno.restaurantreview.data.response.Restaurant
 import com.siaptekno.restaurantreview.data.response.RestaurantResponse
 import com.siaptekno.restaurantreview.data.retrofit.ApiConfig
@@ -45,6 +48,35 @@ class MainActivity : AppCompatActivity() {
         val itemDecoration = DividerItemDecoration(this, layoutManager.orientation)
         binding.rvReview.addItemDecoration(itemDecoration)
         findRestaurant()
+
+        binding.btnSend.setOnClickListener { view ->
+            postReview(binding.edReview.text.toString())
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
+    }
+
+    private fun postReview(review: String) {
+        showLoading(true)
+        val client = ApiConfig.getApiService().postReview(RESTAURANT_ID, "Dicoding", review)
+        client.enqueue(object : Callback<PostReviewResponse> {
+            override fun onResponse(
+                call: Call<PostReviewResponse>,
+                response: Response<PostReviewResponse>
+            ) {
+                showLoading(false)
+                val responseBody = response.body()
+                if (response.isSuccessful && responseBody != null) {
+                    setReviewData(responseBody.customerReviews)
+                } else {
+                    Log.e(TAG, "onFailure: ${response.message()}")
+                }
+            }
+            override fun onFailure(call: Call<PostReviewResponse>, t: Throwable) {
+                showLoading(false)
+                Log.e(TAG, "onFailure: ${t.message}")
+            }
+        })
     }
 
     private fun findRestaurant() {
