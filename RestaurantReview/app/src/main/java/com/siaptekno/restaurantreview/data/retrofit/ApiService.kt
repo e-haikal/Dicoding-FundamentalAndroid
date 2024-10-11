3f44686e1091b0a2536e31dbe3be2b2f6b63cd38
haikal <asmanhaikal@gmail.com>
2024-10-11 11:43:50 +0700

    feat: adding codelab - fetching data review from API

---
+++ RestaurantReview/app/src/main/java/com/siaptekno/restaurantreview/data/retrofit/ApiService.kt
@@ -0,0 +1,12 @@
+package com.siaptekno.restaurantreview.data.retrofit
+
+import com.siaptekno.restaurantreview.data.response.RestaurantResponse
+import retrofit2.Call
+import retrofit2.http.*
+
+interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id: String
+    ): Call<RestaurantResponse>
+}
