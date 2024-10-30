f5b84d6c695efc0593e2508e87fe741821183352
haikal <asmanhaikal@gmail.com>
2024-10-30 13:27:18 +0700

    🔨 fix: fixing the layout mismatch

--- DicodingEventFinal/app/src/main/java/com/siaptekno/dicodingevent/ui/home/HomeFragment.kt
+++ DicodingEventFinal/app/src/main/java/com/siaptekno/dicodingevent/ui/home/HomeFragment.kt
@@ -11,6 +11,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
